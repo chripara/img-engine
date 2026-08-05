4e416d09e5b71b7e9a9ef7d3f2bba12a5e584d58
Christos Paraskevas <cparaskevas91@gmail.com>
2026-08-05 12:58:56 +0300

    Chore several things unused removed and .idea added to gitignore

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from utils.enums.checkpoint import Checkpoint
 
 class Config:
     DEBUG = False
-    CHECKPOINT = Checkpoint.SDXL_BASE  # Checkpoint Selector
 
 class DevelopmentConfig(Config):
     DEBUG = True
