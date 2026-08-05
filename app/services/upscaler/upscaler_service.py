b4d2b991ba13a745aac6875794518d3ad2c984c0
Christos Paraskevas <45561681+chripara@users.noreply.github.com>
2026-08-05 14:55:26 +0300

    Fix index added to upscaler backend for enumeration of results (#21)

--- app/services/upscaler/upscaler_service.py
+++ app/services/upscaler/upscaler_service.py
@@ -9,5 +9,5 @@ def upscale_image(
     imgs: list[Image.Image]
 ) -> list[Image.Image]:
     with UpscalerEngine(req, spec) as engine:
-        images = [engine.upscale_image(img,req) for index ,img in enumerate(imgs)]
+        images = [engine.upscale_image(img, req, index) for index ,img in enumerate(imgs)]
     return images
